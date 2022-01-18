7975ee4e2f0aca315542aa9057fdee31cbaec91d
Itamar-Brito <itamar.brito@gmail.com>
2022-01-18 18:38:31 -0300

    Layout inicial e paleta de cores

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+
+</main>
+
+<footer class="page-footer teal darken-3">
+          <div class="footer-copyright teal darken-3">
+            <div class="container">
+            © 2022 ADLS-Development
+            <a class="grey-text text-lighten-4 right" href="#!">Links</a>
+            </div>
+          </div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        
+    <title>Controle de Pallets</title>
+    <style>
+        body {
+            display: flex;
+            min-height: 100vh;
+            flex-direction: column;
+            }
+
+        main {
+        flex: 1 0 auto;
+        }
+    </style>
+</head>
+<body class="teal lighten-4">
+<header>
+  <div class="row">
+    <div class="col s12 center teal darken-3">
+        <h4 class="teal-text text-lighten-5" >Controle de Pallets CWB</h4>
+    </div>
+</div>  
+</header>
+<main>
+
+
