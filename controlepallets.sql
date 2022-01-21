21fb0e4ecf94ebcecfe619e211c4a7f3c704a9f6
SrLuppo <92128439+SrLuppo@users.noreply.github.com>
2022-01-21 19:00:37 -0300

    tabela sql

--- controlepallets.sql
+++ controlepallets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 21-Jan-2022 às 20:35
+-- Tempo de geração: 21-Jan-2022 às 21:59
 -- Versão do servidor: 5.7.31
 -- versão do PHP: 7.4.9
 
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `registro` (
   `simples` varchar(10) NOT NULL,
   `veiculo` varchar(240) NOT NULL,
   `notafiscal` varchar(240) NOT NULL,
-  `data` date NOT NULL,
+  `data` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 COMMIT;
