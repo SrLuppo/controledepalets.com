421524d366b904f318493163e8e13a89eee9c3a4
Itamar-Brito <itamar.brito@gmail.com>
2022-02-01 22:23:29 -0300

    adicionado insercao na tabela debitos

--- php_actions/addRegister.php
+++ php_actions/addRegister.php
@@ -23,7 +23,35 @@ $veiculo = clear($_POST['placa']) ;
 $nota = clear($_POST['nf']) ; 
 $data = date('Y-m-d H:i:s');
 
+//verificar se já existe a filial na tabela  debitos.
+    $sqlConsultaExists = "SELECT * FROM debitos where filial = '$destino' ";
+    $query = mysqli_query($connect,$sqlConsultaExists);
+    $exist =  mysqli_num_rows( $query ) ;
 
+    if( (bool) $exist ):
+
+        $totais = mysqli_fetch_array( $query );
+
+        $novoVlrEnviadoSimples = intval($totais['enviado_simples']) + intval($simples);
+        $novoVlrEnviadoPbr = intval($totais['enviado_pbr']) + intval($pbr);
+
+        $sqlUpdate = "UPDATE debitos SET enviado_pbr='$novoVlrEnviadoPbr', enviado_simples='$novoVlrEnviadoSimples' WHERE filial='$destino'";
+
+        mysqli_query($connect,$sqlUpdate );
+
+    else:
+        $sqlInsertNewLine = "INSERT INTO debitos (filial, enviado_pbr, enviado_simples) 
+                            VALUES
+                            ('$destino','$pbr','$simples')";
+                        
+        mysqli_query($connect,$sqlInsertNewLine );
+
+    endif;
+
+
+
+
+// Salvamento de histórico
     $sql = "INSERT INTO registro (origem, destino, pbr, simples, veiculo, notafiscal, data)
             VALUES
             ('$origem','$destino','$pbr','$simples','$veiculo','$nota','$data' )";
