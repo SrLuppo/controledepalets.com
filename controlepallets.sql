eaf8c09dc129dbdb20d5730f6c148632f000d451
SrLuppo <92128439+SrLuppo@users.noreply.github.com>
2022-02-01 21:15:44 -0300

    banco atualizado

--- controlepallets.sql
+++ controlepallets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 21-Jan-2022 às 23:43
+-- Tempo de geração: 02-Fev-2022 às 00:14
 -- Versão do servidor: 5.7.31
 -- versão do PHP: 7.4.9
 
@@ -24,6 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `debitos`
+--
+
+DROP TABLE IF EXISTS `debitos`;
+CREATE TABLE IF NOT EXISTS `debitos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filial` varchar(3) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `enviado` int(11) NOT NULL,
+  `devolveu` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `registro`
 --
 
@@ -38,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `registro` (
   `notafiscal` varchar(240) DEFAULT NULL,
   `data` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `registro`
@@ -49,7 +64,21 @@ INSERT INTO `registro` (`id`, `origem`, `destino`, `pbr`, `simples`, `veiculo`, 
 (2, 'CWB', 'POR', '1', '5', 'ADC1245', '1165165', '2022-01-21 20:10:04'),
 (3, 'CWB', 'GRU', '15', '22', 'AKJ4023', '6556565', '2022-01-21 20:29:08'),
 (4, 'CWB', 'GRU', '22', '10', 'ADC1245', '56456465', '2022-01-21 20:36:38'),
-(5, 'CWB', 'GRU', '', '', 'ADC1245', '', '2022-01-21 20:42:33');
+(5, 'CWB', 'GRU', '', '', 'ADC1245', '', '2022-01-21 20:42:33'),
+(6, 'CWB', 'GRU', '10', '11', 'ADC1245', '16519654', '2022-01-24 20:24:38'),
+(7, 'CWB', 'GRU', '44', '23', 'AGP4016', '5466521', '2022-01-24 21:14:56'),
+(8, 'CWB', 'GRU', '44', '23', 'AGP4016', '5466521', '2022-01-24 21:16:31'),
+(9, 'CWB', 'SAO', '55', '30', 'AGP4017', '546652155', '2022-01-24 21:54:30'),
+(10, 'CWB', 'GRA', '44', '77', 'AGP4016', '445', '2022-01-24 21:57:50'),
+(11, 'CWB', 'POT', '48', '60', 'A', '', '2022-01-24 22:01:56'),
+(12, 'CWB', 'GRA', '80', '', 'AGP4017', '564654', '2022-01-24 22:03:58'),
+(13, 'CWB', 'GRU', '15', '', 'AGP4016', '465', '2022-01-24 22:04:54'),
+(14, 'CWB', 'GRU', '15', '', 'AGP4016', '49879', '2022-01-24 22:06:20'),
+(15, 'CWB', 'GRU', '18', '20', 'AGP4016', '79878', '2022-01-24 22:12:21'),
+(16, 'CWB', 'SAO', '50', '80', 'ATY4050', '87987', '2022-01-25 22:27:59'),
+(17, 'CWB', 'GRU', '55', '32', 'ADC1245', '494465', '2022-01-28 21:02:36'),
+(18, 'CWB', 'TDA', '88', '99', 'APT3940', '465646', '2022-01-28 21:34:19'),
+(19, 'CWB', 'TRE', '12', '132', 'ADC1245', '43654', '2022-01-28 21:45:45');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
