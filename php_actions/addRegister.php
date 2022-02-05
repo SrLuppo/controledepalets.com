148fc3d79028328afce2f163dc3705452c4e2c28
Itamar-Brito <itamar.brito@gmail.com>
2022-02-04 23:11:02 -0300

    adicionado timezone

--- php_actions/addRegister.php
+++ php_actions/addRegister.php
@@ -28,6 +28,8 @@ $data = date('Y-m-d H:i:s');
     $query = mysqli_query($connect,$sqlConsultaExists);
     $exist =  mysqli_num_rows( $query ) ;
 
+    date_default_timezone_set('America/Sao_Paulo');
+    
     if( (bool) $exist ):
 
         $totais = mysqli_fetch_array( $query );
